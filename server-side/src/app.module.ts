752f4ee8b37cd6bca92edeadea99ee5143ce6b80
edensmo <edensmo97@gmail.com>
2020-10-27 11:38:07 +0200

    Added Mongo connection

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+// import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [TasksModule]
-})
+  imports: [MongooseModule.forRoot('mongodb+srv://Admin:Aa123456@cluster0.si7to.mongodb.net/cats?retryWrites=true&w=majority'),
+    CatsModule,
+]})
+
+// @Module({
+//   imports: [TasksModule]
+// })
 export class AppModule {}
