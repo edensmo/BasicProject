eac6d0b2a935c4f6c325d0918210f04e1e590e55
unknown <311219661@idf.il>
2020-10-29 17:47:55 +0200

    see that name of all cats and in server added cors

---
+++ client-side/src/CatManager.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from "react";
+import "./CatManager.css";
+
+export default ({ cats = [], onCreateNewCat }) => {
+  const [newCatName, setNewCatName] = useState("");
+
+  const onNewCatNameChange = useCallback((event) => {
+    setNewCatName(event.target.value);
+  }, []);
+
+  const onAddCat = useCallback(
+    (event) => {
+      event.preventDefault();
+      onCreateNewCat(newCatName);
+      setNewCatName("");
+    },
+    [onCreateNewCat, newCatName]
+  );
+
+  return (
+    <div className="container">
+      <section className="cat-create-form">
+        <form>
+          <input
+            type="text"
+            placeholder="Cat name..."
+            value={newCatName}
+            onChange={onNewCatNameChange}
+          />
+          <button type="submit" onClick={onAddCat}>
+            Create
+          </button>
+        </form>
+      </section>
+      <div className="divider" />
+      <section className="cat-list-section">
+        <p>My cat:</p>
+        <ul>
+          {cats.map((cat) => (
+            <li key={cat.name}>{cat.name}</li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 // import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './cats/logger.middleware';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://Admin:Aa123456@cluster0.si7to.mongodb.net/cats?retryWrites=true&w=majority'),
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Admin:Aa123456@cluster0.si7to.mongodb.net/cats?retryWrites=true&w=majority',
+    ),
     CatsModule,
-]})
+  ],
+})
 
 // @Module({
 //   imports: [TasksModule]
