d5dd845db068c40a380b0e5066b1f65137b132f8
unknown <311219661@idf.il>
2020-11-29 15:55:00 +0200

    added popup of add task

--- client-side/src/board/addTask/AddTask.js
+++ client-side/src/board/addTask/AddTask.js
@@ -1,12 +1,55 @@
-import React, { useState } from "react";
+import Axios from "axios";
+import React, { useCallback, useState } from "react";
 import { Button, Form, FormControl, InputGroup, Modal } from "react-bootstrap";
 import "./AddTask.css";
 
+const myServerBaseURL = "http://localhost:3000";
+
 export default function AddTask() {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const nameChange = useCallback((event) => {
+    setName(event.target.value);
+  }, []);
+
+  const descChange = useCallback((event) => {
+    setDescription(event.target.value);
+  }, []);
+
+  const statusChange = useCallback((event) => {
+    setStatus(event.target.value);
+  }, []);
+
+  const addTask = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      name: name,
+      description: description,
+      status: status,
+    };
+    console.log(data);
+
+    await Axios.post(`${myServerBaseURL}/tasks`, data)
+      .then((result) => {
+        console.log("result", result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setStatus("");
+    setName("");
+    setDescription("");
+    handleClose();
+    window.location.reload();
+  };
 
   return (
     <>
@@ -14,15 +57,19 @@ export default function AddTask() {
         הוסף משימה &#43;
       </h1>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
+      <Modal className="modal" show={show} onHide={handleClose}>
+        <Modal.Header className="modal-header">
           <Modal.Title>משימה חדשה</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form className="form-task">
             <Form.Group className="form-group">
               <Form.Label>נושא</Form.Label>
-              <Form.Control className="input-text" placeholder="נושא" />
+              <Form.Control
+                className="input-text"
+                placeholder="נושא"
+                onChange={nameChange}
+              />
             </Form.Group>
 
             <Form.Group>
@@ -31,20 +78,30 @@ export default function AddTask() {
                 className="input-text"
                 as="textarea"
                 placeholder="תיאור"
+                onChange={descChange}
               />
             </Form.Group>
             <Form.Group>
               <Form.Label>סטטוס</Form.Label>
-              <Form.Control className="input-text" placeholder="סטטוס" />
+              <Form.Control
+                className="input-text"
+                placeholder="סטטוס"
+                onChange={statusChange}
+                as="select"
+              >
+                <option>ToDo</option>
+                <option>Doing</option>
+                <option>DONE</option>
+              </Form.Control>
             </Form.Group>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+        <Modal.Footer className="modal-flooter">
+          <Button className="btn" variant="secondary" onClick={handleClose}>
+            ביטול
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button className="btn" variant="primary" onClick={addTask}>
+            שמור
           </Button>
         </Modal.Footer>
       </Modal>
